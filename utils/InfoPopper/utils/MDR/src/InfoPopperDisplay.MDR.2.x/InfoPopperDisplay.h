0e01f2a8c587a4c273bd9fff2ab676587b5f5e68
m_eiman <m_eiman@8f1b5804-98cd-0310-9b8b-c217470a4c7c>
2005-01-27 07:55:46 +0000

    InfoPopper MDR add-on: added. messy notes and code.
    git-svn-id: http://svn.osdrawer.net/imkit@424 8f1b5804-98cd-0310-9b8b-c217470a4c7c

---
+++ utils/InfoPopper/utils/MDR/src/InfoPopperDisplay.MDR.2.x/InfoPopperDisplay.h
@@ -0,0 +1,29 @@
+#ifndef ZOIDBERG_RULE_FILTER_H
+#define ZOIDBERG_RULE_FILTER_H
+/* RuleFilter - performs action depending on matching a header value
+**
+** Copyright 2001 Dr. Zoidberg Enterprises. All rights reserved.
+*/
+
+
+#include <Message.h>
+#include <List.h>
+#include <MailAddon.h>
+
+#include "StringMatcher.h"
+
+class InfoPopperDisplay : public Zoidberg::Mail::Filter {
+	public:
+							InfoPopperDisplay(BMessage *settings);
+		virtual				~InfoPopperDisplay();
+					
+		virtual status_t	InitCheck(BString* out_message = NULL);
+		
+		virtual status_t ProcessMailMessage(BPositionIO** io_message,
+											   BEntry* io_entry,
+											   BMessage* io_headers,
+											   BPath* io_folder,
+											   const char* io_uid);
+};
+
+#endif	/* ZOIDBERG_RULE_FILTER_H */
