f74e7905c4d80bcce5100b1965d33905cb672aee
m_eiman <m_eiman@8f1b5804-98cd-0310-9b8b-c217470a4c7c>
2005-01-28 06:49:52 +0000

    InfoPopper MDR add-on: click-to-open now works
    git-svn-id: http://svn.osdrawer.net/imkit@426 8f1b5804-98cd-0310-9b8b-c217470a4c7c

--- utils/InfoPopper/utils/MDR/src/InfoPopperDisplay.MDR.3.x/InfoPopperDisplay.cpp
+++ utils/InfoPopper/utils/MDR/src/InfoPopperDisplay.MDR.3.x/InfoPopperDisplay.cpp
@@ -7,12 +7,16 @@
 #include <Node.h>
 #include <String.h>
 #include <Entry.h>
+#include <Path.h>
 
 #include <stdlib.h>
 #include <stdio.h>
 
 #include <Message.h>
 #include <Messenger.h>
+
+#include <ChainRunner.h>
+#include <mail_util.h>
 
 #include "InfoPopperDisplay.h"
 
@@ -23,7 +27,75 @@
 
 //class StatusView;
 
-InfoPopperDisplay::InfoPopperDisplay(BMessage *settings) : BMailFilter(settings) {
+extern void SubjectToThread (BString &string);
+extern void extract_address_name(BString &header);
+extern time_t ParseDateWithTimeZone (const char *DateString);
+
+BString generateFileName( BMessage * headers )
+{
+	time_t			dateAsTime;
+	const time_t   *datePntr;
+	ssize_t			dateSize;
+	char			numericDateString [40];
+	struct tm   	timeFields;
+	BString			worker;
+
+	// Generate a file name for the incoming message.  See also
+	// Message::RenderTo which does a similar thing for outgoing messages.
+
+	BString name = headers->FindString("Subject");
+	SubjectToThread (name); // Extract the core subject words.
+	if (name.Length() <= 0)
+		name = "No Subject";
+	if (name[0] == '.')
+		name.Prepend ("_"); // Avoid hidden files, starting with a dot.
+
+	// Convert the date into a year-month-day fixed digit width format, so that
+	// sorting by file name will give all the messages with the same subject in
+	// order of date.
+	if ( !headers->FindString("Date") )
+	{
+		headers->AddString("Date", "Fri, 1 Jan 1970 00:00:00 +0000 CET");
+	}
+	
+	dateAsTime = ParseDateWithTimeZone(headers->FindString("Date"));
+	if (dateAsTime == -1)
+		dateAsTime = time (NULL); // Use current time if it's undecodable.
+	
+	localtime_r(&dateAsTime, &timeFields);
+	sprintf(numericDateString, "%04d%02d%02d%02d%02d%02d",
+		timeFields.tm_year + 1900,
+		timeFields.tm_mon + 1,
+		timeFields.tm_mday,
+		timeFields.tm_hour,
+		timeFields.tm_min,
+		timeFields.tm_sec);
+	name << " " << numericDateString;
+
+	worker = headers->FindString("From");
+	extract_address_name(worker);
+	name << " " << worker;
+
+	name.Truncate(222);	// reserve space for the uniquer
+
+	// Get rid of annoying characters which are hard to use in the shell.
+	name.ReplaceAll('/','_');
+	name.ReplaceAll('\'','_');
+	name.ReplaceAll('"','_');
+	name.ReplaceAll('!','_');
+	name.ReplaceAll('<','_');
+	name.ReplaceAll('>','_');
+	while (name.FindFirst("  ") >= 0) // Remove multiple spaces.
+		name.Replace("  " /* Old */, " " /* New */, 1024 /* Count */);
+
+	return name;
+}
+
+
+InfoPopperDisplay::InfoPopperDisplay(BMessage* settings, BMailChainRunner* runner) 
+: BMailFilter(settings)
+{
+	fPath = runner->Chain()->MetaData()->FindString("path");
 }
 
 InfoPopperDisplay::~InfoPopperDisplay() {
@@ -44,6 +116,9 @@ status_t InfoPopperDisplay::ProcessMailMessage
 	
 	BNode node(entry);
 	
+	io_headers->PrintToStream();
+	printf("path: %s\n", io_folder->Path() );
+	
 	if ( io_headers->FindString("From", &from) != B_OK )
 		from = "<unknown sender>";
 	
@@ -61,6 +136,22 @@ status_t InfoPopperDisplay::ProcessMailMessage
 	entry_ref iconRef;
 	get_ref_for_path("/boot/beos/apps/BeMail",&iconRef);
 	
+	// figure out final name of email
+	BString dest;
+	if ( io_headers->FindString("DESTINATION") )
+	{
+		dest = io_headers->FindString("DESKTIONATION");
+	} else
+	{
+		dest = fPath;
+	}
+	if ( dest.Length() > 0 && dest[dest.Length()-1] != '/' )
+		dest << "/";
+	dest << generateFileName(io_headers);
+	entry_ref mail_ref;
+	BEntry(dest.String()).GetRef(&mail_ref);
+	printf("email path: %s\n", dest.String());
+	
 	// send the message
 	BMessage msg(InfoPopper::AddMessage);
 	msg.AddString("app", "Mail daemon");
@@ -68,6 +159,7 @@ status_t InfoPopperDisplay::ProcessMailMessage
 	msg.AddString("content", text.String());
 	msg.AddRef("iconRef", &iconRef);
 	msg.AddInt32("iconType", InfoPopper::Attribute);
+	msg.AddRef("onClickFile", &mail_ref);
 	
 	BMessenger(InfoPopperAppSig).SendMessage(&msg);
 	
@@ -80,7 +172,7 @@ status_t descriptive_name(BMessage *settings, char *buffer) {
 	return B_OK;
 }
 
-BMailFilter* instantiate_mailfilter(BMessage* settings,BMailChainRunner *)
+BMailFilter* instantiate_mailfilter(BMessage* settings,BMailChainRunner* runner)
 {
-	return new InfoPopperDisplay(settings);
+	return new InfoPopperDisplay(settings,runner);
 }
--- utils/InfoPopper/utils/MDR/src/InfoPopperDisplay.MDR.3.x/InfoPopperDisplay.h
+++ utils/InfoPopper/utils/MDR/src/InfoPopperDisplay.MDR.3.x/InfoPopperDisplay.h
@@ -9,12 +9,13 @@
 #include <Message.h>
 #include <List.h>
 #include <MailAddon.h>
+#include <String.h>
 
 #include "StringMatcher.h"
 
 class InfoPopperDisplay : public BMailFilter {
 	public:
-							InfoPopperDisplay(BMessage *settings);
+							InfoPopperDisplay(BMessage* settings, BMailChainRunner* runner);
 		virtual				~InfoPopperDisplay();
 					
 		virtual status_t	InitCheck(BString* out_message = NULL);
@@ -24,6 +25,9 @@ class InfoPopperDisplay : public BMailFilter {
 											   BMessage* io_headers,
 											   BPath* io_folder,
 											   const char* io_uid);
+
+	protected:
+		BString		fPath;
 };
 
 #endif	/* ZOIDBERG_RULE_FILTER_H */
