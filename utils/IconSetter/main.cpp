7c538ed53264abbaf42743f3ea3bcdade215649e
slaad <slaad@8f1b5804-98cd-0310-9b8b-c217470a4c7c>
2005-02-02 01:17:12 +0000

    IconSetter: Command line utility to set a buddy icon for a user
    git-svn-id: http://svn.osdrawer.net/imkit@453 8f1b5804-98cd-0310-9b8b-c217470a4c7c

---
+++ utils/IconSetter/main.cpp
@@ -0,0 +1,35 @@
+#include <Application.h>
+#include <Entry.h>
+#include <TranslationUtils.h>
+
+#include "IMKitUtilities.h"
+#include <libim/Contact.h>
+
+//#pragma mark -
+
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		printf("Usage is\n\t%s {path to People file} {protocol} {path to image}\n",
+			argv[0]);
+		printf("Note: Protocol can be \"general\" to give a fall back icon\n");
+		return -1;
+	};
+
+	BApplication app("application/x-vnd.BeClan.IMKit.IconSetter");
+
+	entry_ref ref;
+	if (get_ref_for_path(argv[1], &ref) == B_OK) {
+		IM::Contact contact(&ref);
+		BBitmap *originalIcon = BTranslationUtils::GetBitmap(argv[3]);
+		status_t ret = contact.SetBuddyIcon(argv[2], originalIcon);
+		
+		if (ret >= B_OK) {
+			return 0;
+		} else {
+			printf("Error setting icon: %s (%i)\n", strerror(ret), ret);
+		};
+	} else {
+		printf("Couldn't find the People file you specified\n");
+		return -1;
+	};
+};
