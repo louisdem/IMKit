bbbf5588e5479b1322ab8aa524af3d60d4c2d4cf
m_eiman <m_eiman@8f1b5804-98cd-0310-9b8b-c217470a4c7c>
2005-02-02 07:58:56 +0000

    IconSetter: added some error checking and fixed compiler warning
    git-svn-id: http://svn.osdrawer.net/imkit@460 8f1b5804-98cd-0310-9b8b-c217470a4c7c

--- utils/IconSetter/main.cpp
+++ utils/IconSetter/main.cpp
@@ -21,12 +21,19 @@ int main(int argc, char *argv[]) {
 	if (get_ref_for_path(argv[1], &ref) == B_OK) {
 		IM::Contact contact(&ref);
 		BBitmap *originalIcon = BTranslationUtils::GetBitmap(argv[3]);
+		
+		if ( !originalIcon )
+		{
+			printf("Couldn't load image\n");
+			return -1;
+		}
+		
 		status_t ret = contact.SetBuddyIcon(argv[2], originalIcon);
 		
 		if (ret >= B_OK) {
 			return 0;
 		} else {
-			printf("Error setting icon: %s (%i)\n", strerror(ret), ret);
+			printf("Error setting icon: %s (%ld)\n", strerror(ret), ret);
 		};
 	} else {
 		printf("Couldn't find the People file you specified\n");
