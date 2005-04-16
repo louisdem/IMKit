6ada761ee34a787313da533c3ca54904500a908c
slaad <slaad@8f1b5804-98cd-0310-9b8b-c217470a4c7c>
2005-04-16 10:35:35 +0000

    InfoPopper: Can now be used from the command line to send messages to itself. Courtesy mmu_man
    git-svn-id: http://svn.osdrawer.net/imkit@542 8f1b5804-98cd-0310-9b8b-c217470a4c7c

--- utils/InfoPopper/main.cpp
+++ utils/InfoPopper/main.cpp
@@ -26,7 +26,8 @@ class InfoApp : public BApplication {
 				case InfoPopper::AddMessage: {
 					int8 type = 0;
 					if (msg->FindInt8("type", &type) == B_OK) {
-						system_beep(kSoundNames[type]);
+						if (type < (sizeof(kSoundNames)/sizeof(const char *)))
+							system_beep(kSoundNames[type]);
 					};
 					BMessenger(fWin).SendMessage(msg);
 				} break;
@@ -42,6 +43,71 @@ class InfoApp : public BApplication {
 					BApplication::MessageReceived(msg);
 			};
 		};
+		
+		void Usage(void) {
+			printf("InfoPopper [--type <type>] [--app \"App Name\"] [--title \"Message Title\"] [--messageID \"Msg ID\"] [--progress <float>] [--timeout <secs>] [--icon <icon name>] \"Message text\"\n");
+			printf("	<type>: 0 to 3\n");
+		}
+		
+		void ArgvReceived(int32 argc, char **argv) {
+			int i;
+			BMessage msg(InfoPopper::AddMessage);
+			InfoPopper::info_type type = InfoPopper::Information;
+			const char *app = "Command Line";
+			const char *title = "Something Happened";
+			BString content;
+			float progress = 0.0;
+			const char *messageID = NULL;
+			int32 timeout = 0;
+			for (i = 1; i < argc; i++) {
+				//printf("argv[%d] = %s\n", i, argv[i]);
+				if (!strcmp(argv[i], "--help")) {
+					return Usage();
+				} else if (!strcmp(argv[i], "--type")) {
+					if (++i >= argc)
+						return Usage();
+					type = (InfoPopper::info_type)atoi(argv[i]);
+				} else if (!strcmp(argv[i], "--app")) {
+					if (++i >= argc)
+						return Usage();
+					app = argv[i];
+				} else if (!strcmp(argv[i], "--title")) {
+					if (++i >= argc)
+						return Usage();
+					title = argv[i];
+				} else if (!strcmp(argv[i], "--messageID")) {
+					if (++i >= argc)
+						return Usage();
+					messageID = argv[i];
+					msg.AddString("messageID", messageID);
+				} else if (!strcmp(argv[i], "--progress")) {
+					if (++i >= argc)
+						return Usage();
+					progress = atof(argv[i]);
+					msg.AddFloat("progress", progress);
+				} else if (!strcmp(argv[i], "--timeout")) {
+					if (++i >= argc)
+						return Usage();
+					timeout = atol(argv[i]);
+					msg.AddInt32("timeout", timeout);
+				} else if (!strcmp(argv[i], "--icon")) {
+					if (++i >= argc)
+						return Usage();
+					fprintf(stderr, "--icon unimplemented yet\n");
+				} else {
+					if (content.Length())
+						content << "\n";
+					content << argv[i];
+				}
+			}
+			msg.AddInt8("type", (int8)type);
+			msg.AddString("app", app);
+			msg.AddString("title", title);
+			msg.AddString("content", content);
+			//msg.PrintToStream();
+			be_app_messenger.SendMessage(&msg);
+			//BApplication::ArgvReceived(argc, argv);
+		}
 		
 		status_t GetSupportedSuites(BMessage *msg) {
 			msg->AddString("suites", "suite/x-vnd.beclan.InfoPopper");
